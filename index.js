f96e19cf39fddb5138f3d56ea7f8851e19be6914
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-09-23 19:02:48 +0100

    Emit better events

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var emitter = require('emitter');
 var editable = require('editable');
 
 module.exports = makeEditable;
@@ -8,8 +9,10 @@ function makeEditable(elements) {
         edit(element);
     });
 }
+emitter(makeEditable);
 
 function edit(element) {
+    emit('pre-begin-edit', element);
     var value = element.textContent;
     element.innerHTML = '';
     var edit = document.createElement('input');
@@ -17,13 +20,19 @@ function edit(element) {
     edit.value = value;
     element.appendChild(edit);
     edit.focus();
-    editable.emit('begin-edit', element);
     editable.blur(edit, function () {
+        emit('pre-end-edit', element);
         element.innerHTML = edit.value;
         element.setAttribute('data-in-edit-mode', 'false');
         if (value != edit.value) {
-            editable.emit('update', element, edit.value);
+            emit('update', element, edit.value);
         }
-        editable.emit('end-edit', element);
+        emit('post-end-edit', element);
     });
+    emit('post-begin-edit', element);
+}
+
+function emit() {
+    module.exports.emit.apply(module.exports, arguments);
+    editable.emit.apply(editable, arguments);
 }
