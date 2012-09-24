0b412926a3519cd45f96c2afd543b466850f49d0
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-09-24 14:26:41 +0100

    Attempt to improve size maintanence

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ emitter(makeEditable);
 
 function edit(element, options) {
     var dimensions;
+    var oldStyle;
     if (options.maintainSize === true) {
         dimensions = editable.dimensions(element);
     }
@@ -25,9 +26,12 @@ function edit(element, options) {
     edit.value = value;
     element.appendChild(edit);
     if (options.maintainSize === true) {
-        dimensions = editable.transformDimensions(edit, dimensions);
-        edit.style.width = dimensions.width + 'px';
-        edit.style.height = dimensions.height + 'px';
+        var editDimensions = editable.transformDimensions(edit, dimensions);
+        edit.style.width = editDimensions.width + 'px';
+        edit.style.height = editDimensions.height + 'px';
+        oldStyle = {width: element.style.width, height: element.style.height};
+        element.style.width = dimensions.width + 'px';
+        element.style.height = dimensions.height + 'px';
     }
     edit.focus();
     editable.blur(edit, function () {
@@ -35,6 +39,8 @@ function edit(element, options) {
         element.setAttribute('data-in-edit-mode', 'false');
         emit('pre-end-edit', element);
         element.innerHTML = edit.value;
+        element.style.width = oldStyle.width;
+        element.style.height = oldStyle.height;
         if (value != edit.value) {
             emit('update', element, edit.value);
         }
